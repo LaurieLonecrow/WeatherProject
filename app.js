46f6dcfd07716a517a6e7515e2a1e8ba9eea12cc
Laurie Lonecrow <laurieteresa@gmail.com>
2021-10-19 14:26:01 -0400

    add config to gitignore

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function (req, res){
 
 app.post("/", function(req, res){
   const query = req.body.cityName;
-  const apiKey = "";
+  const apiKey = config.apiKey;
   const unit = "imperial"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
 
