c29db101a1a7548fe4e153429eff28842aed0619
Laurie Lonecrow <laurieteresa@gmail.com>
2021-10-19 12:48:26 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const apiKey = "8ba1247de5e4f7d94cc8aa4f75a9581d";
+  const unit = "imperial"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
+
+    https.get(url, function(response){
+
+      response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        // console.log(weatherData)
+        const temp = weatherData.main.temp;
+        const feelsLike = weatherData.main.feels_like;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<h1>The weather in " + query + " is " + feelsLike + " degrees. </h1>")
+        res.write("<h2>with " + weatherDescription + ".<h2>");
+        res.write("<img src=" + imageURL + ">");
+        res.send();
+      });
+  });
+});
+
+
+
+
+app.listen(3001, function(){
+  console.log("Server is running on port 3001.");
+})
